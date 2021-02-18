f1ef5bade371d75573dff4ec49fe5733d0c2c1b1
Rajeshkumar sivaprakasam <rk.sivaprakasam@outlook.com>
2021-02-18 17:38:46 +0530

    Customer Added

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Excercise.Models;
+
+namespace Excercise.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customer = new Customer() { Name = "Sherk!" };
+            return View(customer);
+        }
+    }
+}
